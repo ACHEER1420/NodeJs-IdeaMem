b3687dd0055afc2193173ba2052a0dc66bddfab9
NUTT CHOKWITTAYA <nutt.chokwittaya@gmail.com>
2020-05-31 17:20:23 -0500

    [Handlebars + Bootstrap] - [home + about + _navbar] install handlebars and using bootstrap CDN to create pages and partials(navbar use in every pages)

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
+const exphbr = require('express-handlebars');
+
+// Handlebars Middleware
+app.engine(
+  'handlebars',
+  exphbr({
+    defaultLayout: 'main',
+  })
+);
+app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  const title = 'Welcome to Index Handlebars'
+  res.render('index', {
+    title: title,
+  });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  res.render('about');
 });
 
 const _PORT = 5000;
